8272ca51d3a8d71280c6c2a76c26d8f1dc376b0d
Chandan N <chandan2004.n@gmail.com>
2026-03-17 22:48:59 +0530

    Add recursive GCD function
    Implement GCD calculation using recursion.

---
+++ GCD using recursion.py
@@ -0,0 +1,18 @@
+def findgcd(n1,n2):
+    if n1 == 0:
+        return n2
+    if n1<n2:
+        n1, n2 = n2, n1 # by this we can swap the values of n1 and n2
+    
+    return findgcd((n1%n2),n2) 
+
+num1=int(input("num1 value"))
+num2=int(input("num value"))
+
+result=findgcd(num1,num2)
+print(f"The GCD of {num1} and {num2} is : {result}")
+
+# output:
+# num1: 10
+# num2: 25
+# The GCD of 10 and 25 is : 5
