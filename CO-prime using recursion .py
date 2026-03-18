e92452d516fbb4b7bf9863c73d7f35cea8a04db7
Chandan N <chandan2004.n@gmail.com>
2026-03-18 22:31:59 +0530

    Add coprime function using recursion
    Implement coprime function using recursion to check if two numbers are coprime.

---
+++ CO-prime using recursion .py
@@ -0,0 +1,19 @@
+def coprime(n1,n2):
+    if n1 == 0: #### # if gcd = 1 → coprime
+        return n2 == 1 # only this changd in coprime, leaving all code is exact same as gcd
+    if n1<n2:
+        n1, n2 = n2, n1 # by this we can swap the values of n1 and n2
+    
+    return coprime((n1%n2),n2) 
+
+num1=int(input("num1 value"))
+num2=int(input("num value"))
+
+flag=coprime(num1,num2)
+if flag:
+    print(f" The coprime numbers of {num1} and {num2} is : {flag}")
+else:
+    print("Not a co prime number")
+
+# output:
+#  The coprime numbers of 3 and 5 is : True
